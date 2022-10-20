c526b6fd62bb258a5308e92226b2a6f9ced150d4
Dennis Bui <dennistbui@gmail.com>
2022-10-19 17:25:41 -0700

    05. Image Interpolation Initialization

---
+++ 05.ImageInterpolation/src/ImageInterpolation.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+
+public class ImageInterpolation {
+	//https://stackoverflow.com/questions/32490876/java-how-do-i-get-an-image-to-display
+	public static void main(String[] args) {
+		JFrame F = new JFrame();
+		// Or overload the constructor and give it a title:
+		JFrame F1 = new JFrame("Red Alert!");
+	}
+
+}
